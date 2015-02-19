deeaef6d1c516c4e41a475d5c6ad6e079fea2267
mike <mike@docurated.com>
2015-02-19 13:32:46 -0500

    ETRO-3 access to api limits

---
+++ lib/restforce/middleware/api_usage.rb
@@ -0,0 +1,17 @@
+module Restforce
+    class Middleware::APIUsage < Restforce::Middleware
+        
+        def call(env)
+            api_usage(env[:response_headers])
+            @app.call(env)
+        end
+
+        def api_usage(response)
+            return nil unless response['sforce-limit-info']
+            matches = response['sforce-limit-info'].match(%r{api-usage=(?<used>\d+)/(?<max>\d+)})
+            if matches
+                @client.set_limits(matches['max'].to_i, matches['used'].to_i)
+            end
+        end
+    end
+end
